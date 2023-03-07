c80af83788ceadc5ba28d4a12c45191d938808ed
LeandraVoges <127184530+LeandraVoges@users.noreply.github.com>
2023-03-07 09:16:47 +0100

    Add files via upload

---
+++ Übung1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  6 10:04:19 2022
+
+@author: VOL2SZ
+"""
+
+i = [1,2,3,4]
+j = [2,7]
+
+def zusammenfügen (i, j):
+    h = i + j
+    return h
+
+print (zusammenfügen(i , j))
