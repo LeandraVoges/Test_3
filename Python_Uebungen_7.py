6e617844e12bb944a7b48b471bb3888d6eba18bc
FlorianGuenther2 <Florian.Guenther2@de.bosch.com>
2023-03-07 10:41:27 +0100

    Testnachricht
    Signed-off-by: FlorianGuenther2 <Florian.Guenther2@de.bosch.com>

---
+++ Python_Uebungen_7.py
@@ -0,0 +1,23 @@
+schluessel = int(input("Geben Sie eine Zahl ein, die als Schlüssel fungieren soll (1 - 18): "))
+eingabe = input("Geben Sie ein Wort/einen Satz ein, der verschlüsselt werden soll: ")
+
+def verschluesselung(schluessel, eingabe):
+    ASCII = []
+    x = []
+    for character in eingabe:
+        ASCII.append(ord(character))
+#    print(ASCII)   
+    i = 0
+    while i <= len(ASCII) - 1:
+        ASCII[i] = ASCII[i] + schluessel
+        i += 1
+    print(ASCII)
+    for inhalt in ASCII:
+        x.append(chr(inhalt))
+    verschluesselt = "".join(x)
+    print(x)
+    print(f"Verschlüsselung: {verschluesselt}")
+
+verschluesselung(schluessel, eingabe)
+
+        
